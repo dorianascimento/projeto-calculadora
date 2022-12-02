81d0aa6a082fdb6d62b1a29bf034becf119a4760
dorianascimento <rafaelnd2001@gmail.com>
2022-12-01 22:15:00 -0300

    adicionando função para mostrar qual tecla foi apertada

--- script.js
+++ script.js
@@ -1,113 +1,33 @@
-// variaveis de controle
-let numero = '';
-let numeroTotal;
-let resultadoGeral = 0;
-let equacao = 0;
-let clique = false;
+let numeroTotal = '';
 
-// variaveis de controle de ambiente
-let resultado = document.querySelector('.resultado p');
-let soma = document.querySelector('.somar');
-let sub = document.querySelector('.menos');
-let x = document.querySelector('.vezes');
-let dividir = document.querySelector('.dividir');
 
-// quando clicar nas teclas de números, irá aparecer na tela e guardar em uma variavel de controle
-function teclaCalculadora(num) {
+// função para guardar o numero teclado e mostrar ele na tela
+function mostrarNumero(e) {
+    let telaResultado = document.querySelector('.resultado');
+    let numeroApertado = e.target.getAttribute('data-number');
     
-    if(clique) {
-        resultado.innerHTML = '';
-        clique = false;
+    // se o numero total for menor que o max-digitos, podera ser iserido
+    if(numeroTotal.length < 6) {
+        numeroTotal += `${numeroApertado}`;
     }
 
-    // maximo de 6 caracteres
-    if(numero.length < 6) {
-        numero += num;
-        resultado.innerHTML += num;
-        numeroTotal = Number(numero);
-    }
+    telaResultado.innerHTML = numeroTotal;
 }
 
-// limpar todos elementos da tela
-function limparCalculadora() {
-    numero = '';
-    resultado.innerHTML = '';
-    sub.classList.remove('selecionado');
-    x.classList.remove('selecionado');
-    dividir.classList.remove('selecionado');
-    soma.classList.remove('selecionado');
-    resultadoGeral = 0;
-    numeroTotal = 0;
-    clique = false;
+// adicionando evento de clique nos botoes com numeros
+for(let a = 0; a < 19; a++) {
+    let numerosClique = document.querySelectorAll('.botao')[a];
+    numerosClique.addEventListener('click', (e)=>{
+        numerosClique.classList.add('b-t');
+        setTimeout(()=>{
+            numerosClique.classList.remove('b-t');
+        },500);
+    });
 }
-
-
-function calcular() {
-    switch(equacao) {
-        case 1:
-            soma.classList.remove('selecionado');
-            resultadoGeral += numeroTotal;
-            resultado.innerHTML = resultadoGeral;
-            clique = true;
-            numeroTotal = 0;
-            numero = '';
-            break;
-        case 2:
-            
-            break;
-    }
+for(let i = 0; i < 11; i++) {
+    let numeros = document.querySelectorAll('.botao.numero')[i];
+    numeros.addEventListener('click', mostrarNumero);
 }
 
 
 
-
-
-function somar() {
-    // remover a classe selecionada
-    sub.classList.remove('selecionado');
-    x.classList.remove('selecionado');
-    dividir.classList.remove('selecionado');
-
-    // adicionar a classe que mostra a operação selecionada
-    soma.classList.add('selecionado');
-
-    // toda vez que for cliclado, irá somar o resultado que estava na variavel de controle para uma variavel que guarda o valor total
-    resultadoGeral += numeroTotal;
-    resultado.innerHTML = resultadoGeral;
-
-    equacao = 1;
-    clique = true;
-
-    numeroTotal = 0;
-    numero = '';
-    
-}
-
-function divisao() {
-    sub.classList.remove('selecionado');
-    x.classList.remove('selecionado');
-    soma.classList.remove('selecionado');
-
-    dividir.classList.add('selecionado');
-
-   
-
-}
-
-function multiplicacao() {
-    sub.classList.remove('selecionado');
-    dividir.classList.remove('selecionado');
-    soma.classList.remove('selecionado');
-
-
-    x.classList.add('selecionado');
-}
-
-function subtrair() {
-    x.classList.remove('selecionado');
-    dividir.classList.remove('selecionado');
-    soma.classList.remove('selecionado');
-
-
-    sub.classList.add('selecionado');
-}
